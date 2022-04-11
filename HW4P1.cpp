994e3dfbc56c6a2226278db44b7ae298fc2627de
Dalitso <dalitbanda@gmail.com>
2022-04-11 10:37:13 -0400

    completed HW4P1.cpp

--- HW4P1.cpp
+++ HW4P1.cpp
@@ -6,11 +6,7 @@ using namespace std;
 
 int main()
 {
-    string Name, line;
-    const int count_len = 32;
-    int count_arr[count_len]{0};
-    int index = 0;
-
+    string Name;
     /*****************************************/
     cout << "Student Enter Name" << endl;
     cin >> Name;
@@ -19,46 +15,60 @@ int main()
     cout << "Student Name: " << Name << endl;
     cout << "----------------------------------------------" << endl;
     /*****************************************/
+
     string fileName = "HW4message.txt";
-    ifstream read{fileName};
+    ifstream read{fileName}; // read lines form text file
+
     if (!read)
     {
-        // Print an error and exit
+        // print an error and exit if file could not be opened in the stream
         std::cerr << "message could not be opened for reading!\n";
+        read.close(); // close file stream
         return 1;
     }
     else
     {
         cout << "File is open" << endl;
     }
-
+    // read phrases delimited by '.' and get their sum
+    string line;
+    string line_array[32];
+    const int count_len = 32; // array length
+    int count_arr[count_len]{0};
+    int index = 0; // index of array
     while (getline(read, line, '.'))
     {
         int sum = 0;
         for (int i = 0; i < line.length(); i++)
         {
-            sum += (line.at(i) != ' ') ? line.at(i) : 0;
+            sum += (line.at(i) != ' ') ? line.at(i) : 0; // ignore spaces
         }
 
         if (index < count_len)
         {
-            cout << "Inputting at index: " << index;
-            count_arr[index++] = sum;
-            ;
+            cout << "Inputting at index: " << index << endl;
+            line_array[index] = line;   //insert phrase in string array
+            count_arr[index++] = sum; // insert sum of phrase in sum array and increment index
         }
         else
         {
-            cout << "Memory Full!" << endl;
+            cout << "Memory Full!" << endl; // stop insertion if array is full
+            break;
         }
     }
 
-    cout<<endl;
+    cout << endl;
+    // print sums
     for (int i = 0; i < count_len; i++)
     {
-        if (count_arr[i] == 0)
+        if (count_arr[i] == 0)  //stop when sum is empty
             break;
-        cout << "line: " << i << " sum: " << count_arr[i]<<endl;
+        cout << "line: " << i + 1 << " sum: " << count_arr[i] << endl;
+        cout << "Phrase: " << line_array[i] << endl;
+        cout << "----------------------------------------------" << endl;
     }
+
+    read.close(); // close file stream
 
     return 0;
 }
