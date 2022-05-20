427486e83152315a9961fcab013c7865c085c2e3
Dalitso <dabanda000@citymail.cuny.edu>
2022-05-20 17:16:28 -0400

    Implemented file reading, modification of line 10-13, and printing updated text file

--- FinalP1.cpp
+++ FinalP1.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <cstring>
 #include <fstream>
+#include <map>
 using namespace std;
 
 int main()
@@ -26,35 +27,53 @@ int main()
         return -1;
     }
 
-    while (!stream.eof())
+    while (!stream.eof() && line_count++ <= 40)
     {
         getline(stream, line);
         if (!stream.fail())
         {
-            // cout << line << endl;
-            // line_count++;
 
-            const char *char_line = line.c_str();
-            const int len = strlen(char_line);
-            heap_line = new char[len];
-            for (int i = 0; i < len; i++)
-            {
-                heap_line[i] = char_line[i];
-            }
+            // // const char *c_string = line.c_str();
+            // const int len = strlen(c_string);
+            const int len = line.length();
+            heap_line = new char[len + 1];
 
             for (int i = 0; i < len; i++)
             {
-                cout << heap_line[i];
+                heap_line[i] = line.at(i);
+                // heap_line[i] = c_string[i];
             }
-            cout << endl;
-            //       cout<<myString[i]<<" ";
-            //   }
-            // const char* heap_line = new char;
+
+            heap_line[len] = '\0';
 
             c_string_array.push_back(heap_line);
         }
     }
 
+    int count = 1;
+    for (vector<char *>::iterator iter = c_string_array.begin(); iter != c_string_array.end(); iter++)
+    {
+
+        cout << count++ << ": " << *iter << endl;
+    }
+
+    cout << "\n**********Updated Version (Moved line 10-13)*********" << endl;
+    int first = 9;
+    int last = 13;
+    for (int i = first; i < last; i++)
+    {
+        c_string_array.push_back(c_string_array.at(i));
+    }
+    c_string_array.erase(c_string_array.begin() + first, c_string_array.begin() + last);
+
+    count = 1;
+    for (vector<char *>::iterator iter = c_string_array.begin(); iter != c_string_array.end(); iter++)
+    {
+
+        cout << count++ << ": " << *iter << endl;
+    }
+
+    // cout << "Line count is: " << line_count << endl;
     stream.close();
 
     return 0;
